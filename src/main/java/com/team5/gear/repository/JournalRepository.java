e2f1b766974267e255cd520418b17dc56c4205b6
Choi_Hanna <hannachoe53@gmail.com>
2025-11-02 04:18:34 +0900

    Revert to commit 8ea7025

--- src/main/java/com/team5/gear/repository/JournalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team5.gear.repository;
-
-import com.team5.gear.entity.Journal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JournalRepository extends JpaRepository<Journal, Long> {
-}
